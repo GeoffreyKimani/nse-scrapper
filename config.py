667e5a982e544427b25169a882215196ca2c5d29
GeoffreyKimani <jeffkim207@gmail.com>
2022-06-10 12:47:59 +0300

    Added constant and configuration files

---
+++ config.py
@@ -0,0 +1,29 @@
+import os
+from constants import SECRET_KEY, SECURITY_PASSWORD_SALT, DATABASE_URI
+
+
+class BaseConfig(object):
+    DEBUG = False
+    TESTING = False
+    CSRF_ENABLED = True
+    SECRET_KEY = os.environ.get(SECRET_KEY) or os.urandom(32)
+    SECURITY_PASSWORD_SALT = os.environ.get(SECURITY_PASSWORD_SALT) or os.urandom(32)
+    SQLALCHEMY_DATABASE_URI = os.environ.get(DATABASE_URI)
+
+
+class ProductionConfig(BaseConfig):
+    DEBUG = False
+
+
+class StagingConfig(BaseConfig):
+    DEVELOPMENT = True
+    DEBUG = True
+
+
+class DevelopmentConfig(BaseConfig):
+    DEVELOPMENT = True
+    DEBUG = True
+
+
+class TestingConfig(BaseConfig):
+    TESTING = True
