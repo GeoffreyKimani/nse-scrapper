b0298e88f16b2d100353457c54e07189f1e461f5
GeoffreyKimani <jeffkim207@gmail.com>
2022-06-10 12:31:39 +0300

    Database config

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
+# create flask app instance
 app = Flask(__name__)
+app.debug = True
+
+# Database configuration
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://jeffkim:kim@localhost:5432/nse_data'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+
+db = SQLAlchemy(app)
 
 
 @app.route('/')
